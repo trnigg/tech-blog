bc447127fd7a9f50c134282bf4439f6d53f16771
trnigg <tim@nigg.com.au>
2023-12-02 10:47:06 +0800

    Added model relationships

---
+++ models/index.js
@@ -0,0 +1,23 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+// A user can have many posts and comments.
+User.hasMany(Post, { foreignKey: 'user_id', onDelete: 'CASCADE' });
+User.hasMany(Comment, { foreignKey: 'user_id', onDelete: 'CASCADE' });
+
+// A post and comment belong to a user.
+Post.belongsTo(User, { foreignKey: 'user_id', onDelete: 'CASCADE' });
+Comment.belongsTo(User, { foreignKey: 'user_id', onDelete: 'CASCADE' });
+
+// A post can have many comments.
+Post.hasMany(Comment, { foreignKey: 'post_id', onDelete: 'CASCADE' });
+
+// A comment belongs to a post.
+Comment.belongsTo(Post, { foreignKey: 'post_id', onDelete: 'CASCADE' });
+
+// Relationships are cascading, so:
+// If a user is deleted, all of their posts and comments will be deleted.
+// If a post is deleted, all of its comments will be deleted as well.
+// TODO - reconsider this approach:
+// maybe it makes more sense to User's posts and comments to remain, but be "uncredited"?
