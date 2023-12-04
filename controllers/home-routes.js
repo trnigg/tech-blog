617e83899cd6f87ba4cf43b42316af6ec555d7ca
trnigg <tim@nigg.com.au>
2023-12-04 11:32:45 +0800

    Added post-page route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const { User, Post } = require('../models'); // Confirm the models I need here later
+const { User, Post, Comment } = require('../models');
 
 // Routes to handle navigation/rendering of pages
 
@@ -33,6 +33,24 @@ router.get('/sign-up', async (req, res) => {
     res.render('sign-up');
   } catch (err) {
     console.error(err); // Log  err
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// GET a single Post by Post ID
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [{ model: User }, { model: Comment }],
+    });
+    if (!postData) {
+      res.status(404).json({ message: 'No Post found with this id!' });
+      return;
+    }
+    const post = postData.get({ plain: true }); // Need to map?
+    res.render('post', { post });
+  } catch (err) {
+    console.error(err);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
