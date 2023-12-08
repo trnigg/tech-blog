fd8771b2742fe4a23f8175c77eee8d8c15f02e9d
trnigg <tim@nigg.com.au>
2023-12-08 17:41:59 +0800

    Added user AUth utility

---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+const userAuth = (req, res, next) => {
+  // If the user is not logged in, redirect the user to the login page
+  if (!req.session.logged_in) {
+    res.redirect('/'); // Confirm/change url for redirect
+  } else {
+    // If the user is logged in, execute the route function requested (i.e. 'next')
+    next();
+  }
+};
+
+module.exports = userAuth;
