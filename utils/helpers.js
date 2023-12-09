dd184c3fe5cf17205165022c70836a3c9ea7aa7c
trnigg <tim@nigg.com.au>
2023-12-09 12:55:08 +0800

    Refactor date formatting in format_date function

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,9 @@
 module.exports = {
   format_date: (date) => {
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
-    return date.toLocaleDateString('en-UK');
+    return date.toLocaleDateString('en-UK', {
+      day: '2-digit',
+      month: '2-digit',
+      year: '2-digit',
+    });
   },
 };
