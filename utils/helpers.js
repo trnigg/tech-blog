0e3df4a19cd184e903b56dbd5afecfe5f9dc6046
trnigg <tim@nigg.com.au>
2023-12-09 15:27:11 +0800

    add line break preservation util

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,9 +1,14 @@
 module.exports = {
-  format_date: (date) => {
+  formatDate: (date) => {
     return date.toLocaleDateString('en-UK', {
       day: '2-digit',
       month: '2-digit',
       year: '2-digit',
     });
+  },
+  preserveLineBreaks: (text) => {
+    // see https://www.geeksforgeeks.org/how-to-replace-line-breaks-with-br-tag-using-javascript/
+    // essentially replaces newline characters of all operating systems with <br> tags
+    return text.replace(/(?:\r\n|\r|\n)/g, '<br>');
   },
 };
